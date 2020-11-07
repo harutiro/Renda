c615cb8cde4e48f8a03ba23a560082185d8d9b71
harutiro <harutiro2027@outlook.jp>
2020-11-08 02:36:14 +0900

    vor 1.2 時間制限を追加 しかし、謎のエラーにより起動不可

--- app/src/main/java/app/makino/harutiro/renda/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/renda/MainActivity.kt
@@ -2,23 +2,60 @@ package app.makino.harutiro.renda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    val tapButton = findViewById<Button>(R.id.tapButton)
+    val startButton = findViewById<Button>(R.id.startButton)
+    val countText = findViewById<TextView>(R.id.countText)
+    val secondLabelText = findViewById<TextView>(R.id.secondLabelText)
+
     var tapCount = 0
+    var second = 10
+
+    val timer : CountDownTimer = object : CountDownTimer(10000,1000){
+        override fun onFinish() {
+            startButton.isVisible = true
+            tapButton.setBackgroundResource(R.drawable.background_rounded_circle_glay)
+            second = 10
+            tapCount = 0
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+            tapButton.setBackgroundResource(R.drawable.background_rounded_circle)
+            second = second - 1
+            secondLabelText.text = second.toString()
+
+        }
+
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tapButton = findViewById<Button>(R.id.tapButton)
-        val countText = findViewById<TextView>(R.id.countText)
 
+
+        startButton.setOnClickListener {
+            countText.text = tapCount.toString()
+            startButton.isVisible = false
+
+            timer.start()
+        }
 
         tapButton.setOnClickListener{
-            tapCount = tapCount + 1
-            countText.text = tapCount.toString()
+            if(second < 10){
+                tapCount = tapCount + 1
+                countText.text = tapCount.toString()
+            }
+
 
         }
+
+
 
 
     }
